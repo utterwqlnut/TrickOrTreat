deb029306927110625e7a2ff8dde456811133ba1
Dhruva Chayapathy <dhruva.chayapathy@gmail.com>
2022-10-01 23:53:00 -0400

    first README

---
+++ README.md
@@ -0,0 +1,2 @@
+# TrickOrTreat
+This project is a mobile app that lets you hold trick or treat competitions with your friends.
---
+++ android/app/src/main/kotlin/com/example/trickortreat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trickortreat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
